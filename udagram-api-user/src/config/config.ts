eac520680afac8f6fd0b06b1004f73826d19495f
DaoQuyenGithub <dquyen2806@gmail.com>
2022-11-11 12:45:12 +0700

    push

---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': '12345678',
+  'database': 'postgres',
+  'host': 'project3udacity.c29prjujuffn.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'arn:aws:s3:::test-698161925490-dev',
+  'url': 'http://a3ae1ea4b5d7e4eacbeb4ea90843f64e-1124954678.us-east-1.elb.amazonaws.com:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
